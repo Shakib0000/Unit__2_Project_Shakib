734fe4326ea6c65a566d7c9ecad5c69780e2d077
Shakib <shakiba4@nycstudents.net>
2023-10-11 02:10:35 -0400

    finished unit 2 project for now

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class EquationRunner {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
